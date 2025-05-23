156789312cb2fc37fd5a2f715c9066d59024ef55
jay <jsureliya732@rku.ac.in>
2025-05-24 00:13:08 +0530

    vite

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // or the correct base path
   plugins: [react(), tailwindcss()],
 })
